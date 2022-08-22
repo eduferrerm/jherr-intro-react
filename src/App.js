d3f9916fe6b048e284bb68157956d062f551a2b9
Eduardo Ferrer <ekferrerm@gmail.com>
2022-08-22 14:57:08 -0400

    feat(text-change-test-deploy): testing deployment updates

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning React Dude!
         </a>
       </header>
     </div>
