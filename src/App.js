33de4632c2897820b0b41f239cce2eba03b0d29e
Eduardo Ferrer <ekferrerm@gmail.com>
2022-08-23 11:47:55 -0400

    feat(end-videos-1-4): finished videos 1-4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import pokemon from './pokemon.json'
+import PropTypes from 'prop-types';
+
+const PokemonRow = ({pokemonItem}) => (
+  <tr>
+    <td>{pokemonItem.name.english}</td>
+    <td>{pokemonItem.type.join(', ')}</td>
+  </tr>
+)
+
+PokemonRow.propTypes = {
+  pokemonItem: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string
+    }),
+    type: PropTypes.arrayOf(PropTypes.string)
+  })
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learning React Dude!
-        </a>
-        <h2>Test</h2>
-      </header>
+    <div style={{
+      margin: '0 auto',
+      maxWidth: 800,
+      padding: '1rem'
+    }}>
+      <h1 className="title">Pokemon Search</h1>
+      <table width="100%">
+        <thead>
+          <tr style={{
+            fontWeight: 'bold'
+          }}>
+            <td>Name</td>
+            <td>Type</td>
+          </tr>
+        </thead>
+        <tbody>
+          {pokemon.slice(0,20).map((pokemonItem)=> (
+            <PokemonRow
+              key={pokemonItem.id}
+              pokemonItem={pokemonItem}
+            />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
